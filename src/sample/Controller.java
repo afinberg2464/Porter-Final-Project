472b4c59ecca69d3133c3df9059f40f796f328f1
andrewfinberg <afinberg2464@students.mc3.edu>
2019-11-09 11:13:07 -0500

    Made functioning buttons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,23 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
 public class Controller {
 
+    @FXML
+    protected Button ringButton;
+    @FXML
+    protected Button quitButton;
+
+    public void ringBell() {
+        System.out.println("Sup?");
+    }
+
+    public void quitProgram() {
+        System.exit(1);
+    }
 }
